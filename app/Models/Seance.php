1f62080b29ea708871cc54510b4c9547ae5bd77d
BornetFloryan <floryan.bornet@gmail.com>
2025-12-29 15:36:25 +0100

    Breeze

---
+++ app/Models/Seance.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Seance extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'date',
+        'note',
+        'user_id',
+    ];
+
+}
